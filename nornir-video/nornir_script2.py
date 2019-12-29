332eb1573628ac82e442d4c3ca80124944a24dad
IPvZero <46572101+IPvZero@users.noreply.github.com>
2019-12-29 21:15:47 +0000

    Create nornir_script2.py

---
+++ nornir-video/nornir_script2.py
@@ -0,0 +1,25 @@
+from nornir import InitNornir
+from nornir.plugins.tasks.networking import netmiko_send_command
+from nornir.plugins.functions.text import print_result
+import colorama
+from colorama import Fore, Style
+
+print("\n")
+print(Fore.YELLOW + "#" * 70 + Style.RESET_ALL)
+print(" " * 20 + "Welcome to " + Fore.RED + "Nornir!" + Style.RESET_ALL)
+print(" " * 15 + "This is a Dynamic script!")
+print("\n Please enter the commands you wish to execute, seperated by commas")
+print("Example: " + Fore.GREEN + "< show ip int brief, show ip route, show version >")
+print(Fore.YELLOW + "#" * 70 + Style.RESET_ALL)
+commands = input ("\nEnter Commands: ")
+cmds = commands.split(",")
+
+for cmd in cmds:
+    nr = InitNornir()
+
+    result = nr.run(
+        task=netmiko_send_command,
+        command_string=cmd
+        )
+
+    print_result(result)
