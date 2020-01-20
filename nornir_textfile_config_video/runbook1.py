4c0265c0e25294a1f3966e077417d9664a6c14b5
IPvZero <46572101+IPvZero@users.noreply.github.com>
2020-01-20 22:45:46 +0000

    Create runbook1.py

---
+++ nornir_textfile_config_video/runbook1.py
@@ -0,0 +1,16 @@
+from nornir import InitNornir
+from nornir.plugins.functions.text import print_result, print_title
+from nornir.plugins.tasks.networking import netmiko_send_config, netmiko_send_command
+
+nr = InitNornir(
+    config_file="config.yaml", dry_run=True
+)
+
+def baseconfig(ipvzero):
+    ipvzero.run(task=netmiko_send_config, config_file= "config_textfile")
+    ipvzero.run(task=netmiko_send_command, command_string = "show ip int brief")
+
+results = nr.run(task = baseconfig)
+
+print_title("DEPLOYING AUTOMATED BASELINE CONFIGURATIONS")
+print_result(results)
