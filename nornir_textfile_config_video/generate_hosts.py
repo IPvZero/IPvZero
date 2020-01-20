68c994da4a42089248eb64902ea67f9cc149b56b
IPvZero <46572101+IPvZero@users.noreply.github.com>
2020-01-20 22:48:31 +0000

    Create generate_hosts.py

---
+++ nornir_textfile_config_video/generate_hosts.py
@@ -0,0 +1,12 @@
+print("---")
+print("\n")
+
+for i in range (1,11:
+    print("R" + str(i)+ ":")
+    print("    hostname: 192.168.1." + str(i))
+    print("    platform: 'ios'")
+    print("    username: john")
+    print("    password: cisco")
+    print("    groups:")
+    print("        - cisco_group")
+    print("\n")
