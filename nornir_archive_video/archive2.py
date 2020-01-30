232d008b5206ee1131cb18567513361def217392
IPvZero <46572101+IPvZero@users.noreply.github.com>
2020-01-30 12:41:53 +0000

    Create archive2.py

---
+++ nornir_archive_video/archive2.py
@@ -0,0 +1,28 @@
+from nornir import InitNornir
+from nornir.plugins.tasks import networking
+from nornir.plugins.functions.text import print_result
+from nornir.plugins.tasks.files import write_file
+from datetime import date
+import pathlib
+
+def backup_configurations(task):
+    config_dir = "config-archive"
+    date_dir = config_dir + "/" + str(date.today())
+    pathlib.Path(config_dir).mkdir(exist_ok=True)
+    pathlib.Path(date_dir).mkdir(exist_ok=True)
+    r = task.run(task=networking.napalm_get, getters=["config"])
+    task.run(
+        task=write_file,
+        content=r.result["config"]["running"],
+        filename=f"" + str(date_dir) + "/" + task.host.name + ".txt",
+    )
+
+
+nr = InitNornir(config_file="config.yaml")
+
+
+result = nr.run(
+    name="Creating Backup Archive", task=backup_configurations
+)
+
+print_result(result)
