4223288e45faaa3387dc947927d9e620ce489652
IPvZero <46572101+IPvZero@users.noreply.github.com>
2020-02-06 16:32:56 +0000

    Create structured.py

---
+++ nornir_textFSM_video/structured.py
@@ -0,0 +1,8 @@
+from nornir import InitNornir
+from nornir.plugins.tasks.networking import netmiko_send_command
+from nornir.plugins.functions.text import print_result
+nr = InitNornir("config.yaml")
+
+results = nr.run(netmiko_send_command, command_string="show interface switchport", use_textfsm=True)
+
+print_result(results)
