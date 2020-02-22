88c2c933c4e21c3a61b65aea21d0ae5dd9e725bd
IPvZero <46572101+IPvZero@users.noreply.github.com>
2020-02-22 22:08:07 +0000

    Create interface.py

---
+++ Nornir-Genie-Parsers/interface.py
@@ -0,0 +1,24 @@
+from nornir import InitNornir
+from nornir.plugins.tasks.networking import netmiko_send_command
+from nornir.plugins.functions.text import print_result
+
+def get_facts(task):
+    r = task.run(netmiko_send_command, command_string="show interfaces", use_genie=True)
+    task.host["facts"] = r.result
+    for i in range(0,3):
+        for j in range(0,4):
+            broadcast_value = int(task.host['facts']['GigabitEthernet' + str(i) + "/" + str(j)]['counters']['in_broadcast_pkts'])
+            if broadcast_value >= 5000:
+                print("Potential broadcast storm on " + task.host.hostname + "'s GigabitEthernet" + str(i) + "/" + str(j) + " interface")
+
+
+
+def main() -> None:
+    nr = InitNornir(config_file="config.yaml")
+    result = nr.run(task=get_facts)
+    #print_result(result)
+    #import ipdb;
+    #ipdb.set_trace()
+
+if __name__ == '__main__':
+    main()
