dbf0d1b115014ba5956a7e081418d61a28f5ec55
IPvZero <46572101+IPvZero@users.noreply.github.com>
2020-02-22 22:07:44 +0000

    Create debugger.py

---
+++ Nornir-Genie-Parsers/debugger.py
@@ -0,0 +1,17 @@
+from nornir import InitNornir
+from nornir.plugins.tasks.networking import netmiko_send_command
+from nornir.plugins.functions.text import print_result
+
+def get_facts(task):
+    r = task.run(netmiko_send_command, command_string="show interfaces", use_genie=True)
+    task.host["facts"] = r.result
+
+def main() -> None:
+    nr = InitNornir(config_file="config.yaml")
+    result = nr.run(task=get_facts)
+    print_result(result)
+    import ipdb;
+    ipdb.set_trace()
+
+if __name__ == '__main__':
+    main()
